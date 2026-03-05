e059068e4c41dee5197100004253a47acf5c6a46
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2026-03-05 20:43:05 -0300

    Create ex002.py

---
+++ ex002.py
@@ -0,0 +1,5 @@
+# Desafio 002: Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas
+
+nome = input('Digite seu nome: ')
+
+print('Olá, '+ nome + '. Seja bem vindo(a)!')
