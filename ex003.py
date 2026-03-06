067acd3f7d09f32a2c5b8635b8b6b1d4be286eee
Ana Letícia Cabral do Rosário <cabraldorosarioanaleticia@gmail.com>
2026-03-05 21:31:50 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,7 @@
+# Desafio 003: Crie um programa que leia dois números e mostre a soma entre eles.
+
+numero_1 = int(input('Digite o primeiro número: '))
+numero_2 = int(input('Digite o segundo número: '))
+soma = numero_1 + numero_2
+
+print('A soma entre o número {} e o número {} é: {}'.format(numero_1, numero_2, soma))
